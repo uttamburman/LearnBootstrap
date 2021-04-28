20a00bd45ef4abc6b1816341fc66dcb5e412415e
uttamburman <57400191+uttamburman@users.noreply.github.com>
2021-04-28 19:12:23 +0530

    Update Webscript.js
    Dynamic form updates

--- Webscript.js
+++ Webscript.js
@@ -1,12 +1,12 @@
-
-
 $(document).ready(function(){
     $('.batchOffering').keypress(function (e) {
       if (e.which == 13) {
         var i=$('.batchOffering').val();
         console.log(i);
+        $('.schedules_wrapper').append("<div class=\"row\"><div class=\"col\"><label for=\"batchSchedule\">Day</label></div><div class=\"col\"><label for=\"batchSchedule\">Time</label></div></div>");
         while(i>0){
-          $('.schedules_wrapper').append("<div class=\"row\"><div class=\"col\"><label for=\"batchSchedule\""+i+">No. of Batch Offering</label><div class=\"form-group col-md-10\"><input type=\"text\" class=\"form-control batchSchedule\""+i+" id=\"batchOffering\"></div></div><div class=\"col\"></div></div");
+          
+          $('.schedules_wrapper').append("<div class=\"row\"><div class=\"col\"><div class=\"form-group col-md-10\"><input type=\"text\" class=\"form-control batchSchedule"+i+"\" id=\"batchOffering\"></div></div><div class=\"col\"><div class=\"form-group col-md-10\"><input type=\"text\" class=\"form-control batchSchedule"+i+"\" id=\"batchOffering\"></div></div> <button class=\"btn btn-light\" type=\"button\" onClick=\"addBatch("+i+")\">Add</button><button class=\"btn btn-light\" type=\"button\">Delete</button></div");
           i--;
         }
         
@@ -14,6 +14,4 @@ $(document).ready(function(){
       } 
     });
 });
-
-
 
